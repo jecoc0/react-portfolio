c4ee8324d3bf851328a77e531716df013ea3e510
Jessica <sevorg.jess@gmail.com>
2022-04-07 21:01:13 -0600

    add picture

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import styled from 'styled-components'
+import JG from '../assets/images/JG.jpg'
+
+const AboutBlock = styled.p`
+  margin: 20px;
+  margin-top: 100px;
+`
+
+const AboutPhoto = styled.img`
+  margin: 50px;
+  width: 200px;
+`
+
+const AboutDiv = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`
+
 
 function About() {
   return (
     <section>
       <h1 id="about">Who am I?</h1>
+      <AboutDiv>
+        <AboutPhoto src={JG}></AboutPhoto>
+        <AboutBlock>My Name is Jessica
+          <br />
+          I am a web developer with a background in finance
+          <br />
+          My hobbies include quilting, powerlifting, and running
+          <br />
+          Currently, I am obsessed with the Netflix show, Bridgerton
+        </AboutBlock>
+      </AboutDiv>
     </section>
   )
 }
